e3a26f2b1d9f3fba3c044df603d1cccd968537af
Oh-Hun Kwon <kirloyal@gmail.com>
2018-06-24 23:20:32 +0200

    UI to tune params

--- annotator.py
+++ annotator.py
@@ -30,19 +30,6 @@ class Window(QtGui.QDialog):
 		self.canvas = FigureCanvas(self.figure)
 		self.toolbar = NavigationToolbar(self.canvas, self)
 
-		# # Just some button connected to `plot` method
-		# self.btnSync = QtGui.QPushButton('Sync')
-		# self.btnSync.clicked.connect(self.sync)
-		
-		# self.btnPlay = QtGui.QPushButton('play')
-		# self.btnPlay.clicked.connect(self.play)
-		
-		# self.btnStop = QtGui.QPushButton('stop')
-		# self.btnStop.clicked.connect(self.stop)
-	
-		# self.btnLoad = QtGui.QPushButton('Load')
-		# self.btnLoad.clicked.connect(self.load)
-
 		self.btnCorner = QtGui.QPushButton('Corner')
 		self.btnCorner.setFixedWidth(100)
 		self.btnCorner.clicked.connect(self.GetCorner)
@@ -61,30 +48,105 @@ class Window(QtGui.QDialog):
 		# self.listFile = QtGui.QListWidget()
 		# self.listFile.installEventFilter(self)
 		# self.listFile.setFixedWidth(100)
-		
+
+		self.cbGray = QtGui.QCheckBox("Gray")
+		self.cbGray.stateChanged.connect(lambda:self.evCheckBox(self.cbGray))
+
+		self.slGray = QtGui.QSlider(Qt.Horizontal)
+		self.slGray.setMinimum(0)
+		self.slGray.setMaximum(10)
+		self.slGray.setValue(0)
+		self.slGray.setTickPosition(QtGui.QSlider.TicksBelow)
+		self.slGray.setTickInterval(1)
+		self.slGray.setFixedWidth(100)
+		self.slGray.valueChanged.connect(lambda:self.evSlider(self.slGray))
+
+		self.cbHarris = QtGui.QCheckBox("Harris")
+		self.cbHarris.stateChanged.connect(lambda:self.evCheckBox(self.cbHarris))
+
+		self.slHarris = QtGui.QSlider(Qt.Horizontal)
+		self.slHarris.setMinimum(0)
+		self.slHarris.setMaximum(10)
+		self.slHarris.setValue(0)
+		self.slHarris.setTickPosition(QtGui.QSlider.TicksBelow)
+		self.slHarris.setTickInterval(1)
+		self.slHarris.setFixedWidth(100)
+		self.slHarris.valueChanged.connect(lambda:self.evSlider(self.slHarris))
+
+		self.cbHough = QtGui.QCheckBox("Hough Line")
+		self.cbHough.stateChanged.connect(lambda:self.evCheckBox(self.cbHough))
+
+		self.slHough = QtGui.QSlider(Qt.Horizontal)
+		self.slHough.setMinimum(0)
+		self.slHough.setMaximum(10)
+		self.slHough.setValue(0)
+		self.slHough.setTickPosition(QtGui.QSlider.TicksBelow)
+		self.slHough.setTickInterval(1)
+		self.slHough.setFixedWidth(100)
+		self.slHough.valueChanged.connect(lambda:self.evSlider(self.slHough))
+
+		layoutControl = QtGui.QGridLayout()
+		layoutControl.addWidget(self.cbGray,0,0,1,1)
+		layoutControl.addWidget(self.slGray,1,0,1,1)
+		layoutControl.addWidget(self.cbHarris,2,0,1,1)
+		layoutControl.addWidget(self.slHarris,3,0,1,1)
+		layoutControl.addWidget(self.cbHough,4,0,1,1)
+		layoutControl.addWidget(self.slHough,5,0,1,1)
 
 		layout = QtGui.QGridLayout()
-
 		layout.addWidget(self.toolbar,0,0,1,4)
-		layout.addWidget(self.canvas,1,1,3,3)
-		layout.addWidget(self.btnCorner,1,0,2,1)
-		layout.addWidget(self.btnClick,2,0,2,1)
-		
-		# layout.addWidget(self.edt,2,0,1,4)
-		# layout.addWidget(self.btnSync,2,0,1,1)
-		# layout.addWidget(self.btnFuse,3,0,1,1)
-		
-		# layout.addWidget(self.btnGenerate,5,0,1,1)
-		# layout.addWidget(self.listFile,2,1,4,1)
+		layout.addWidget(self.canvas,1,1,4,3)
+		layout.addWidget(self.btnCorner,1,0,1,1)
+		layout.addLayout(layoutControl,2,0,1,1)
 		
 
 		self.setLayout(layout)
-		# self.lsMp4 = []
-		# self.dictWav = {}
-		# self.bClick = False
-		# self.lsSplitPosition = []
 		self.ax = self.figure.add_subplot(111)
 		self.figure.tight_layout()
+
+		self.bGray = False
+		self.bHarris = False
+		self.bHough = False
+		self.imgHarris = False
+		self.imgHough = False
+
+		self.testInit()
+
+	def evCheckBox(self, cb):
+		if cb.text() == "Gray":
+			self.bGray = cb.isChecked()
+		if cb.text() == "Harris":
+			self.bHarris = cb.isChecked()
+		if cb.text() == "Hough Line":
+			self.bHough = cb.isChecked()		
+		self.getFusedImg()
+	
+	def evSlider(self, sl):
+		self.getFusedImg()	
+
+
+	def getFusedImg(self):
+		ret = np.zeros(self.gray.shape)
+		
+		if self.bGray:
+			ret = ret + float(self.slGray.value())/10 * self.gray
+
+		if self.bHarris:
+			ret = ret + float(self.slHarris.value())/10 * self.imgHarris
+
+		if self.bHough:
+			ret = ret + float(self.slHough.value())/10 * self.imgHough
+
+		
+		xlim = self.ax.get_xlim()
+		ylim = self.ax.get_ylim()
+		self.ax.clear()
+		self.ax.imshow(ret, cmap='gray', vmin = 0, vmax = 255)
+		self.ax.set_xlabel(self.file)
+		self.ax.set_xlim(xlim)
+		self.ax.set_ylim(ylim)
+		self.canvas.draw()
+
 
 	def eventFilter(self, obj, event):
 		if event.type() == QEvent.KeyPress and obj == self.listFile:
@@ -112,24 +174,28 @@ class Window(QtGui.QDialog):
 	def dropEvent(self, event):
 		if len(event.mimeData().urls()) != 1:
 			return
-		file = unicode(event.mimeData().urls()[0].toLocalFile()) 
+		self.file = unicode(event.mimeData().urls()[0].toLocalFile()) 
 		_, strExtension = os.path.splitext(file)
+
 		if strExtension.lower() == ".mp4":
-			cap = cv2.VideoCapture(file)
+			cap = cv2.VideoCapture(self.file)
 			w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH ))
 			h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT ))
 			if cap.isOpened():
 				ret, frame = cap.read()
 				if ret == True:
 					self.img = frame
+					self.gray = cv2.cvtColor(self.img,cv2.COLOR_BGR2GRAY)
 		elif strExtension.lower() == ".jpg":
 			pass
 		else:
 			return
+
 		self.ax.clear()
 		self.ax.imshow(self.img)
 		self.ax.set_xlabel(file)
 		self.canvas.draw()
+
 
 
 	def GetCorner(self):
@@ -174,9 +240,9 @@ class Window(QtGui.QDialog):
 		        y1 = int(y0 + 3000*(a))
 		        x2 = int(x0 - 3000*(-b))
 		        y2 = int(y0 - 3000*(a))
-		        cv2.line(grayLine,(x1,y1),(x2,y2),255.0)
+		        cv2.line(grayLine,(x1,y1),(x2,y2),1.0)
 		        ret = ret + grayLine
-		ret = ret/ret.max()*255
+		ret = ret/ret.max() * 255
 		return ret
 
 
@@ -218,8 +284,24 @@ class Window(QtGui.QDialog):
 		X = self.figure.ginput(1)[0]
 		self.edt.appendPlainText(str(X))
 		return X
-		
 
+	def testInit(self):
+		self.file = 'mp4/test1_cam1.MP4'
+		cap = cv2.VideoCapture(self.file)
+		w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH ))
+		h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT ))
+		if cap.isOpened():
+			ret, frame = cap.read()
+			if ret == True:
+				self.img = frame
+				self.gray = cv2.cvtColor(self.img,cv2.COLOR_BGR2GRAY)
+				self.imgHarris = self.GetCornersHarris(self.gray)
+				self.imgHough = self.GetLinesHough(self.gray)
+
+		self.ax.clear()
+		self.ax.imshow(self.img)
+		self.ax.set_xlabel(self.file)
+		self.canvas.draw()
 
 if __name__ == '__main__':
 	app = QtGui.QApplication(sys.argv)
